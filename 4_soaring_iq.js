f00d9e1c2235c2cfcb1d28baa973d5653f0c82c3
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-05-09 19:35:55 -0600

    done

--- 4_soaring_iq.js
+++ 4_soaring_iq.js
@@ -1,1 +1,16 @@
 // Your time at the Dojo will definitely make you smarter! Let’s say a new Dojo student, Bogdan entered with a modest IQ of 101. Let’s say that during a 14-week bootcamp, his IQ rose by .01 on the first day, then went up by an additional .02 on the second day, then up by .03 more on the third day, etc. all the way until increasing by .98 on his 98th day (the end of 14 full weeks). Writing a function given a day and a starting IQ, return Bogdan's IQ on that specific day
+
+function soaringIQ() {
+    var iq = 101;
+    var increment = .01;
+    for (var i = 1; i <= 98; i++) {
+        iq = iq + increment;
+        increment = increment + .01
+        console.log("Day " + i + ": IQ of " + iq);
+    }
+    console.log("Bogdan's IQ would be " + iq + " at the end of the bootcamp.");
+}
+
+soaringIQ()
+
+
--- 5_letter_grade.js
+++ 5_letter_grade.js
@@ -1,1 +1,48 @@
 // Mr. Cerise teaches high school math. Write a function that assigns and prints a letter grade, given an integer representing a score from 0 to 100? Those getting 90+ get an ‘A’, 80-89 earn ‘B’, 70-79 is a ‘C’, 60-69 should get a ‘D’, and lower than 60 receive ‘F’. For example, given 88, you should log "Score: 88. Grade: B". Given the score 61, log the string "Score: 61. Grade: D".
+
+function letterGrade(grade) {
+    if (grade >= 90) {
+        return "Score: " + grade + "%. Grade: A"
+    }
+    if (grade >= 80 && grade <= 89) {
+        return "Score: " + grade + "%. Grade: B"
+    }
+    if (grade >= 70 && grade <= 79) {
+        return "Score: " + grade + "%. Grade: C"
+    }
+    if (grade >= 60 && grade <= 69) {
+        return "Score: " + grade + "%. Grade: D"
+    }
+    if (grade <= 59) {
+        return "Score: " + grade + "%. Grade: F"
+    }
+}
+
+console.log(letterGrade(95));
+console.log(letterGrade(85));
+console.log(letterGrade(75));
+console.log(letterGrade(65));
+console.log(letterGrade(55));
+
+// another solution
+
+// function letterGrade(score){
+//     if(score < 60){
+//         console.log("Score: " + score + ". Grade: F")
+//     }
+//     else if(score < 70){
+//         console.log("Score: " + score + ". Grade: D")
+//     }
+//     else if(score < 80){
+//         console.log("Score: " + score + ". Grade: C")
+//     }
+//     else if(score < 90){
+//         console.log("Score: " + score + ". Grade: B")
+//     }
+//     else{
+//         console.log("Score: " + score + ". Grade: A")
+//     }
+// }
+
+// letterGrade(88);
+// letterGrade(61);
